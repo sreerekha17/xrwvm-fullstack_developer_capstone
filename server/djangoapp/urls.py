1e280818b85ac2cbacf32251296fc3374e1632b0
sreerekha17 <rekha.sree17@gmail.com>
2024-02-18 20:31:03 -0500

    feat: week2- part2

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -11,7 +11,8 @@ urlpatterns = [
     # path for login
     path(route='login', view=views.login_user, name='login'),
 
-    # path(route='login', view=views.login_user, name='login'),
+    # path for login
+    path(route='logout', view=views.logout_user, name='logout'),
 
     # path for dealer reviews view
 
