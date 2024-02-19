fc72c1e5ba46f3e3b720c28a922edaacceb8c3ae
sreerekha17 <rekha.sree17@gmail.com>
2024-02-18 21:09:13 -0500

    feat: week2 - part3

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('contact/', TemplateView.as_view(template_name="Contact.html")),
     path('about/', TemplateView.as_view(template_name="About.html")),
     path('admin/', admin.site.urls),
+    path('register/', TemplateView.as_view(template_name="index.html")),
     path('djangoapp/', include('djangoapp.urls')),
     path('', TemplateView.as_view(template_name="Home.html")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
